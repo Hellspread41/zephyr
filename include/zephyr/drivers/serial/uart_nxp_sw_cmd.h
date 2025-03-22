5301e4c50e0aef0039b8080446e681307031b29b
Simon Marceau <marceau.simon@socabelec.com>
2025-03-22 17:59:14 +0100

    Add header file for the sw commands used by the uart_drv_cmd() api.
    Signed-off-by: Simon Marceau <marceau.simon@socabelec.com>

---
+++ include/zephyr/drivers/serial/uart_nxp_sw_cmd.h
@@ -0,0 +1,25 @@
+/*
+* Copyright 2017,2021,2023-2024 NXP
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+/**
+* @file
+*
+* @brief Public header file for the commands defined to manage
+* the single wire mode for nxp kinetis lpuart driver.
+*/
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_UART_NXP_SW_CMD_H
+#define ZEPHYR_INCLUDE_DRIVERS_UART_NXP_SW_CMD_H
+
+
+
+
+
+
+
+
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_UART_NXP_SW_CMD_H */
